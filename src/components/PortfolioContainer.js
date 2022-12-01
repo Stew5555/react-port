e84df4a05498216219abee22a010eac0d879323e
Stew5555 <104962519+Stew5555@users.noreply.github.com>
2022-11-30 21:47:57 -0800

    started on portfolioContainer

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+
+export default function PortfolioContainer() {
+    (
+        <div>
+
+        </div>
+    )
+}
