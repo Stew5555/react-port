6cd00f73c0499f7265a237d68f5abae5611b3e71
Stew5555 <104962519+Stew5555@users.noreply.github.com>
2022-11-30 22:15:43 -0800

    started on protfolio file

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,3 +3,23 @@ import '../../styles/style.css';
 import Header from '../Header';
 import Footer from '../Footer';
 import portrait from '../../assets/me.jpeg';
+
+export default function About() {
+    return (
+        <div>
+                  <Header />
+      <div className="content">
+        <h2 id="h2">About me</h2>
+        <img className="image" ></img>
+        <div className="bio">
+          <p>I am passionate about strengthening my skills.</p>
+          <p>Currently Learing how to code and make websites at UCSD bottcamp!</p>
+          <p>I plan to use my coding skills to make amazing digital products that are user-friendly to all.</p>
+          <p>I live in the beautiful san Diego, where its always sunny with nice weather, and pretty beaches .</p>
+          <p>If you have any questions Email me!</p>
+        </div>
+      </div>
+      <Footer />
+        </div>
+    )
+}
---
+++ src/components/pages/Portfolio.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import '../../styles/style.css';
+import Footer from '../Footer';
+import Header from '../Header';
+import Project from '../Project';
