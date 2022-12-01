c8995220db5add389dcf108c22e7f28d35b85eef
Stew5555 <104962519+Stew5555@users.noreply.github.com>
2022-11-30 21:34:47 -0800

    added some project pics in asset folder

--- src/components/NavTab.js
+++ src/components/NavTab.js
@@ -3,32 +3,32 @@ import React from 'react';
 //destructuring object aka props. pass in props to every functional component
 //props are currentpage and handlePageChange - those get passed in
 
-function NavTabs({ currentPage, PageChange }) {
-  return (
- 
-    <ul className="nav nav-tabs" >
-      <li className="nav-item">
-        <a
-          href="#about"
-          onClick={() => PageChange('About')}
-          //ternary operator. if current page is home, add "active" class with nav-link
-          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-        >
-          About me
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#portfolio"
-          onClick={() => PageChange('Portfolio')}
-          className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
-        >
-          Portfolio
-        </a>
-      </li>
-    </ul>
- 
-  );
+function NavTabs({ currentPage, handlePageChange }) {
+    return (
+
+        <ul className="nav nav-tabs" style={{ display: 'flex', justifyContent: 'flex-end', borderBottom: 'none' }}>
+            <li className="nav-item">
+                <a
+                    href="#about"
+                    onClick={() => handlePageChange('About')}
+                    //ternary operator. if current page is home, add "active" class with nav-link
+                    className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+                >
+                    About me
+                </a>
+            </li>
+            <li className="nav-item">
+                <a
+                    href="#portfolio"
+                    onClick={() => handlePageChange('Portfolio')}
+                    className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+                >
+                    Portfolio
+                </a>
+            </li>
+        </ul>
+
+    );
 }
 
 export default NavTabs;
