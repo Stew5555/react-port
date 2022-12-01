2889b69f5e9e2bbcc047967fc66a5365baf79f0e
Stew5555 <104962519+Stew5555@users.noreply.github.com>
2022-11-30 21:21:40 -0800

    added a header func in portfolio file

---
+++ src/components/pages/Portfolio.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/style.css';
+
+
+export default function Header() {
+    return (
+        <div className="hero">
+
+            <h1 className="title" >
+
+            </h1>
+            <p className="title" >
+                Coder.  Web-Developer
+            </p>
+        </div>  
+    );
+}
