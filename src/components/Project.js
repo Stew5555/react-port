b39299f81a35dc8a3eaa5685f292e4af23937628
Stew5555 <104962519+Stew5555@users.noreply.github.com>
2022-11-30 21:42:00 -0800

    setting up Project file

---
+++ src/components/Project.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/style.css';
+import gitlogo from '../assets/GitHub-Mark-32px.png';
+
+export default function Project(props) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
