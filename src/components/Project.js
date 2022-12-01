af61d684e2010b3c03eb4bf2ed4ceeb86388ba5b
Stew5555 <104962519+Stew5555@users.noreply.github.com>
2022-11-30 21:45:30 -0800

    added links and images in project file

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,8 +4,16 @@ import gitlogo from '../assets/GitHub-Mark-32px.png';
 
 export default function Project(props) {
     return (
-        <div>
-            
+        <div className='col-md-6'>
+                        <a href={props.project} alt={props.projectAlt}><img className="one-project image" src={props.img} alt={props.imgAlt}></img></a>
+             <div className="one-project">
+                <p className="space-above"><a className="project-link" href={props.project} alt={props.projectAlt}><b>{props.projectTitle}</b></a></p>
+            </div>
+            <div>
+                
+            </div>
+                 <p className="space-below">{props.description}
+                 </p>
         </div>
     )
 }
